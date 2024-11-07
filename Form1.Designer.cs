101d3e98ec94198ecd5486b867d7ad77551ccb07
Gonz�lez S�nchez Ferdinand Gerard <GS24015@ti.ues.edu.sv>
2024-11-07 13:34:55 -0600

    Ajuste

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -34,9 +34,10 @@ namespace Clave5_Grupo6
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(13, 24);
+            this.button1.Font = new System.Drawing.Font("Microsoft Tai Le", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(3, 12);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(95, 30);
             this.button1.TabIndex = 0;
             this.button1.Text = "Conexión";
             this.button1.UseVisualStyleBackColor = true;
