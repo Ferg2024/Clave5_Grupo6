030500df5186f060bce482f6ae35778598a69a1d
Gonz�lez S�nchez Ferdinand Gerard <GS24015@ti.ues.edu.sv>
2024-11-07 13:29:21 -0600

    Conexión a la BD

--- Form1.cs
+++ Form1.cs
@@ -7,14 +7,51 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace Clave5_Grupo6
 {
     public partial class Form1 : Form
     {
+        //Creacion de variables estaticas de clase 
+
+        static string Servidor = "localhost"; //Nombre o ip del servidor de MySQL
+        static string BD = "clave5_grupodetrabajodb6"; //Nombre de la base de datos
+        static string Usuario = "root"; //Usuario de acceso a MySQL
+        static string Paswoord = "root"; //Contraseña de usuario de acceso a MySQL
+
+         //Crearemos la cadena de conexión concatenando las variables
+
+        static string cadenaConexion = "Database=" + BD + "; Data Source=" + Servidor + "; User Id = " + Usuario + "; Password=" + Paswoord + "";
+        //Instancia para conexión a MySQL, recibe la cadena de conexión
+        static MySqlConnection conexionBD = new MySqlConnection(cadenaConexion);
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+              conexionBD.Open(); //se abre la conexion de la variable global declara enla parte superior del formulario
+              MessageBox.Show("Conexión exitosa!","Información", MessageBoxButtons.OK, MessageBoxIcon.Information);// se manda un mensaje de estado de conexion
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex);
+            }
+            finally
+            {
+                conexionBD.Close(); // se cierra la conexion
+            }
         }
     }
 }
